17ce693fbf83902075e9002421cbe761646e0b74
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-01-10 11:43:05 +0100

    klalr

---
+++ Prov repetiton e prov/Program.cs
@@ -0,0 +1,27 @@
+﻿int hp = 10;
+
+
+
+
+
+
+static void Ritahp(int hp)
+{
+
+    System.Console.Write("[");
+    for (int i = 0; i < hp; i++)
+    {
+        System.Console.Write("=");
+    }
+    System.Console.WriteLine("]");
+}
+
+
+while (hp > 0)
+{
+    Ritahp(hp);
+    hp--;
+}
+
+
+Console.ReadLine();
